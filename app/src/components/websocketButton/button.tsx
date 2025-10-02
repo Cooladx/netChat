d1abecb6b164002dca4401f58eb5b7ea87e9b549
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-10-02 14:50:48 -0400

    added folder for handling components

---
+++ app/src/components/websocketButton/button.tsx
@@ -0,0 +1,31 @@
+import * as signalR from "@microsoft/signalr"; 
+
+
+export default function Button() {
+    const connection = new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:5019/hub")
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+    
+    async function start() {
+        try {
+            await connection.start();
+            console.log("SignalR Connected.");
+        } catch (err) {
+            console.log(err);
+            setTimeout(start, 5000);
+        }
+    };
+    
+    connection.onclose(async () => {
+        await start();
+    });
+    
+    // Start the connection.
+    // start();
+    return (
+    <>
+    <button type="button" onClick={start}> Websocket Connect</button>
+    </>
+    )
+}
