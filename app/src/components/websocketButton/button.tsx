9334dae4918adeec272fcf948d2a99d58e776e7f
Mike Nguyen <111297303+cooladx@users.noreply.github.com>
2025-10-07 14:05:24 -0400

    button to connect to websocket and fixed issue of rerendering connection object.

--- app/src/components/websocketButton/button.tsx
+++ app/src/components/websocketButton/button.tsx
@@ -1,24 +1,36 @@
 import * as signalR from "@microsoft/signalr";
-
-export default function Button() {
-  const connection = new signalR.HubConnectionBuilder()
-    .withUrl("http://localhost:5019/hub")
-    .configureLogging(signalR.LogLevel.Information)
-    .build();
-
+import { HubConnection } from "@microsoft/signalr";
+export default function Button({
+  connection,
+  setConnection,
+}: {
+  connection: signalR.HubConnection | null;
+  setConnection: React.Dispatch<React.SetStateAction<HubConnection | null>>;
+}) {
   async function start() {
+    if (connection != null) {
+      console.log("you already have a connection!");
+      return;
+    }
+    const socketConnection = new signalR.HubConnectionBuilder()
+      .withUrl("http://localhost:5019/hub")
+      .configureLogging(signalR.LogLevel.Information)
+      .build();
+
+    setConnection(socketConnection);
+    socketConnection.onclose(async () => {
+      await start();
+    });
+
     try {
-      await connection.start();
+      await socketConnection.start();
       console.log("SignalR Connected.");
+      console.log(socketConnection);
     } catch (err) {
       console.log(err);
       setTimeout(start, 5000);
     }
   }
-
-  connection.onclose(async () => {
-    await start();
-  });
 
   return (
     <>
