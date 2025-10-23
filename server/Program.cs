66d244a84809b8b03fc277d86034893612a635e3
JosephaoSuny <179733322+JosephaoSuny@users.noreply.github.com>
2025-10-23 15:20:52 -0400

    Establish DB connection using Npgsql

--- server/Program.cs
+++ server/Program.cs
@@ -1,10 +1,12 @@
 using System.Text.Json.Serialization;
-using netChat.Hubs;
+using Microsoft.EntityFrameworkCore;
+using netChat;
+using Npgsql;
 
 var builder = WebApplication.CreateSlimBuilder(args);
 
 // debugging for terminal.
-builder.Logging.SetMinimumLevel(LogLevel.Debug); 
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 
 // Add debugging for SignalR and json serialization.
 builder.Services.AddSignalR(options =>
@@ -13,9 +15,19 @@ builder.Services.AddSignalR(options =>
 }).AddJsonProtocol(options =>
 {
     options.PayloadSerializerOptions.PropertyNamingPolicy = null;
-    options.PayloadSerializerOptions.TypeInfoResolver = new netChat.NetChatContext();
+    options.PayloadSerializerOptions.TypeInfoResolver = new NetChatContext();
 });
 
+// Sets up our connection to the database
+var sql_client_builder = new NpgsqlConnectionStringBuilder
+{
+    Host = "localhost:5432",
+    Username = "postgres",
+    Password = "<password>",
+    Database = "netChatDB"
+};
+
+builder.Services.AddDbContext<NetChatDBContext>(options => options.UseNpgsql(sql_client_builder.ConnectionString));
 
 builder.Services.AddCors(options =>
 {
@@ -29,7 +41,6 @@ builder.Services.AddCors(options =>
         });
 });
 
-
 // Builds web app
 var app = builder.Build();
 
@@ -42,7 +53,7 @@ app.UseCors();
 
 app.MapControllers();
 // Links url with endpoint hub for websocket.
-app.MapHub<ChatHub>("/hub");
+app.MapHub<netChat.Hubs.ChatHub>("/hub");
 
 
 // Starts application and only stops till it's shut down manually.
@@ -57,4 +68,8 @@ namespace netChat
     [JsonSerializable(typeof(int))]
     [JsonSerializable(typeof(bool))]
     public partial class NetChatContext : JsonSerializerContext { }
+
+    public class NetChatDBContext : DbContext
+    {
+    }
 }
